f9357a54b61e37c577abd5ffcc083bf94260f5ac
Travell Hector <travell.hector@gmail.com>
2018-04-28 09:09:35 -0700

    first push

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+
+var app = express();
+
+
+var PORT = process.env.PORT || 8080;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+//Server Map 
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+// LISTENER
+
+app.listen(PORT, function() {
+  console.log("App listening");
+});
